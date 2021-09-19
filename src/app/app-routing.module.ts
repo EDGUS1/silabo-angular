f55d4cbc44876d297d45d2229e194ffb3833d699
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-09-19 12:19:36 -0500

    chore: add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeSilabosComponent } from './components/home-silabos/home-silabos.component';
 import { LoginComponent } from './components/login/login.component';
+import { SilaboComponent } from './components/silabo/silabo.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginComponent,
+  },
+  {
+    path: 'silabo',
+    component: HomeSilabosComponent,
+  },
+  {
+    path: 'silabo/:id',
+    component: SilaboComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
   },
 ];
 
