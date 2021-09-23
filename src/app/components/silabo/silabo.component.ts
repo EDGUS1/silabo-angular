d6684bedd4aba0b33aee04afab69efda2c330a3d
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-09-23 12:17:32 -0500

    feat: add component silabo

--- src/app/components/silabo/silabo.component.ts
+++ src/app/components/silabo/silabo.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  faTrashAlt,
+  faHeart,
+  faPen,
+  faFileDownload,
+} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-silabo',
   templateUrl: './silabo.component.html',
-  styleUrls: ['./silabo.component.css']
+  styleUrls: ['./silabo.component.css'],
 })
 export class SilaboComponent implements OnInit {
+  faHeart = faHeart;
+  faTrashAlt = faTrashAlt;
+  faPen = faPen;
+  faFileDownload = faFileDownload;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
