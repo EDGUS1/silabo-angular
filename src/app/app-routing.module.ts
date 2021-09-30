f7f8e7bb52709a6385efbc964e14b35326e2b0bc
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-09-30 13:50:14 -0500

    feat: add guard to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,24 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeSilabosComponent } from './components/home-silabos/home-silabos.component';
 import { LoginComponent } from './components/login/login.component';
 import { SilaboComponent } from './components/silabo/silabo.component';
+import { AuthGuard } from './guards/auth.guard';
+import { NoauthGuard } from './guards/noauth.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginComponent,
+    canActivate: [NoauthGuard],
   },
   {
     path: 'silabos',
     component: HomeSilabosComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'silabo',
     component: SilaboComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: '**',
