648902e8cf6d378bb002f53287b0b83fa3cd04a9
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-12-26 22:27:20 -0500

    chore: update routes

--- src/app/silabo/silabo.module.ts
+++ src/app/silabo/silabo.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { SilaboRoutingModule } from './silabo-routing.module';
 import { CardSilaboComponent } from './components/card-silabo/card-silabo.component';
 import { CardSilabosListComponent } from './components/card-silabos-list/card-silabos-list.component';
-import { FormSilaboComponent } from './components/form-silabo/form-silabo.component';
+import { FormSilaboComponent } from './pages/form-silabo/form-silabo.component';
 import { HeaderSilaboComponent } from './components/header-silabo/header-silabo.component';
 import { HomeSilabosComponent } from './pages/home-silabos/home-silabos.component';
 import { NewCourseComponent } from './components/new-course/new-course.component';
@@ -15,6 +15,10 @@ import { SearchPipe } from './pipes/search.pipe';
 import { TruncatePipe } from './pipes/truncate.pipe';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ReferenceComponent } from './components/reference/reference.component';
+import { ListCardComponent } from './components/list-card/list-card.component';
+import { NewUnidadComponent } from './components/new-unidad/new-unidad.component';
+import { VerSemanaComponent } from './components/ver-semana/ver-semana.component';
+import { AgregarSemanaComponent } from './components/agregar-semana/agregar-semana.component';
 
 @NgModule({
   declarations: [
@@ -28,6 +32,10 @@ import { ReferenceComponent } from './components/reference/reference.component';
     TruncatePipe,
     SearchPipe,
     ReferenceComponent,
+    ListCardComponent,
+    NewUnidadComponent,
+    VerSemanaComponent,
+    AgregarSemanaComponent,
   ],
   imports: [
     CommonModule,
