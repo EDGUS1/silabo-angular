dce7b592b4eb8bb6b7535775d8a78bddef55496f
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2022-02-17 00:41:52 -0500

    chore: Add local endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,10 @@
 
 export const environment = {
   production: false,
-  api: { baseUrl: 'https://api-silabo.herokuapp.com/api/v1' },
+  api: {
+    baseUrl: 'https://api-silabo.herokuapp.com/api/v1',
+    // baseUrl: 'http://localhost:3000/api/v1',
+  },
 };
 
 /*
