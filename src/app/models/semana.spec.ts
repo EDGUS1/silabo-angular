f3baac470c6549ad9f927f29c5b50381fb50a6be
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2022-02-26 19:55:17 -0500

    feat: Create semana model

---
+++ src/app/models/semana.spec.ts
@@ -0,0 +1,7 @@
+import { Semana } from './semana';
+
+describe('Semana', () => {
+  it('should create an instance', () => {
+    expect(new Semana()).toBeTruthy();
+  });
+});
---
+++ src/app/models/semana.ts
@@ -0,0 +1,10 @@
+export class Semana {
+  teoria: string;
+  laboratorio: string;
+  actividades_async: any[];
+  recursos_async: any[];
+  estrategias_async: any[];
+  actividades: any[];
+  recursos: any[];
+  estrategias: any[];
+}
