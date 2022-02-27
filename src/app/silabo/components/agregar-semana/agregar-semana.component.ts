bf6ad9f0ab073c731d6934cad7eda90e839d2e68
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2022-02-26 20:00:44 -0500

    feat: Implement funcionality for Semana

--- src/app/silabo/components/agregar-semana/agregar-semana.component.ts
+++ src/app/silabo/components/agregar-semana/agregar-semana.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { AgregarElementoCardComponent } from '../agregar-elemento-card/agregar-elemento-card.component';
 
 @Component({
   selector: 'app-agregar-semana',
@@ -7,10 +14,30 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./agregar-semana.component.css'],
 })
 export class AgregarSemanaComponent implements OnInit {
+  @Input() fromParent;
+  @ViewChildren(AgregarElementoCardComponent)
+  childrenElement: QueryList<AgregarElementoCardComponent>;
+
+  teoria: string;
+  laboratorio: string;
   constructor(public activeModal: NgbActiveModal) {}
 
   ngOnInit(): void {}
   closeModal(sendData) {
     this.activeModal.close(sendData);
+  }
+
+  agregarSemana() {
+    this.closeModal({
+      unidad_id: this.fromParent.unidad_id,
+      teoria: this.teoria,
+      laboratorio: this.laboratorio,
+      actividades_async: this.childrenElement.toArray()[0].elementos,
+      recursos_async: this.childrenElement.toArray()[1].elementos,
+      estrategias_async: this.childrenElement.toArray()[2].elementos,
+      actividades: this.childrenElement.toArray()[3].elementos,
+      recursos: this.childrenElement.toArray()[4].elementos,
+      estrategias: this.childrenElement.toArray()[5].elementos,
+    });
   }
 }
